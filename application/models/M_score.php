f4fb93c04f118a96d3d0d820e54ddd62911b659d
Adi Nugroho <65408099+adissikoko10@users.noreply.github.com>
2022-01-04 08:56:19 +0700

    Update M_score.php

--- application/models/M_score.php
+++ application/models/M_score.php
@@ -12,31 +12,31 @@ class M_score extends CI_Model{
 	// }
 
 	function get_all_data_score(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_ISP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_ISP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}
 
 	function get_all_data_medan(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IMO' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IMO' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}
 
 	function get_all_data_jakarta(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IJO' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IJO' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}
 
 	function get_all_data_paritohan(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IPP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_IPP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}
 	
 	function get_all_data_tg(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_TG' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Astinet_TG' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}
 	function get_all_data_iisp(){
-		$query = "SELECT  ROUND(SUM(download)/10) AS total_download, ROUND(SUM(upload)/10) AS total_upload, tanggal FROM tbl_score WHERE nama='Indosat_ISP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
+		$query = "SELECT  ROUND(SUM(download)/COUNT(download)) AS total_download, ROUND(SUM(upload)/COUNT(upload)) AS total_upload, tanggal FROM tbl_score WHERE nama='Indosat_ISP' GROUP BY tbl_score.tanggal ORDER BY tbl_score.tanggal ASC";
         return $this->db->query($query)->result_array();
 	}	
 
