04f2bc3620e23086f492291ea53c7ef8ba74e0e7
Adi Nugroho <65408099+adissikoko10@users.noreply.github.com>
2022-07-12 16:42:38 +0700

    v5

--- application/views/v_score.php
+++ application/views/v_score.php
@@ -1,59 +1,75 @@
 <!-- ======= Score Assessment ======= -->
 <style>
-    .active{
-    color: #fff;
-    background: #008FFB;
-    border: 1px solid blue;
-    border-bottom: 2px solid blue;
+    .active {
+        color: #fff;
+        background: #008FFB;
+        border: 1px solid blue;
+        border-bottom: 2px solid blue;
     }
+
     button {
-    background: #fff;
-    color: #222;
-    border: 1px solid #e7e7e7;
-    border-bottom: 2px solid #ddd;
-    border-radius: 2px;
-    padding: 4px 17px;
-}
+        background: #fff;
+        color: #222;
+        border: 1px solid #e7e7e7;
+        border-bottom: 2px solid #ddd;
+        border-radius: 2px;
+        padding: 4px 17px;
+    }
 </style>
 <section id="portfolio" class="portfolio">
     <div class="container" data-aos="fade-up">
         <div class="row" data-aos="fade-up" data-aos-delay="100">
             <div class="section-title">
                 <h2>Bandwidth Astinet ISP (300 mbps)</h2>
-            </div>   	
-      
-            <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-            
-            <body>
-            <div id="chart">
-                <div class="toolbar">
-                    
-                    <button id="one_months" class="active">
-                        1 Bulan
-                    </button>
-                    
-                    <button id="three_months">
-                        3 Bulan
-                    </button>
-                    
-                    <button id="six_months">
-                        6 Bulan
-                    </button>
-
-                    <button id="one_year">
-                        1 Tahun
-                    </button>
-                    
-                    <button id="all">
-                        ALL
-                    </button>
-                </div>
-                <div id="chart-timeline">
-                </div>
             </div>
-            
-            
-            <script src="theme/js/jquery.min.js"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+
+            <body>
+                <div id="chart">
+
+                    <div class="toolbar">
+                        <button id="one_months" class="active">
+                            1 Bulan
+                        </button>
+
+                        <button id="three_months">
+                            3 Bulan
+                        </button>
+
+                        <button id="six_months">
+                            6 Bulan
+                        </button>
+
+                        <button id="one_year">
+                            1 Tahun
+                        </button>
+
+                        <button id="all">
+                            ALL
+                        </button>
+                        <select id="select" class="form-group col-md-3">
+                            <option value="01">Pilih Bulan !</option>
+                            <option value="01">Januari</option>
+                            <option value="02">Februari</option>
+                            <option value="03">Maret</option>
+                            <option value="04">April</option>
+                            <option value="05">Mei</option>
+                            <option value="06">Juni</option>
+                            <option value="07">Juli</option>
+                            <option value="08">Agustus</option>
+                            <option value="09">September</option>
+                            <option value="10">Oktober</option>
+                            <option value="11">November</option>
+                            <option value="12">Desember</option>
+                        </select>
+                    </div>
+                    <div id="chart-timeline">
+                    </div>
+                </div>
+
+
+                <script src="theme/js/jquery.min.js"></script>
 
                 <script>
                     var download = [];
@@ -61,35 +77,62 @@
                     var down = [];
                     var up = [];
 
-                    function backDays(days){
-                    var now = new Date();
-                    return now.setDate(now.getDate() - days);
+                    function backDays(days) {
+                        var now = new Date();
+                        return now.setDate(now.getDate() - days);
                     }
 
-                    function getFirstDayInMonth(){
-                    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-                    return new Date(y, m - 1, 1).getTime();
+                    function getFirstDateOfMonth(month) {
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            mon = new Date(y + '-' + month + '-01'),
+                            m = mon.getMonth();
+                        // console.log(m)
+                        return new Date(y, m, 1).getTime();
                     }
 
-                    function getLastDayInMonth(){
-                    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-                    return new Date(y, m + 1 - 1, 0).getTime();
+                    function getLastDateOfMonth(month) {
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            mon = new Date(y + '-' + month + '-01'),
+                            m = mon.getMonth();
+                        return new Date(y, m + 1, 0).getTime();
+                    }
+
+                    function getFirstDayInMonth() {
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            m = date.getMonth();
+                        return new Date(y, m - 1, 1).getTime();
+                    }
+
+                    function getLastDayInMonth() {
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            m = date.getMonth();
+                        return new Date(y, m + 1 - 1, 0).getTime();
                     }
 
                     function getFirstDayPreviousMonth(totalMonths = 1) {
-                    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-                    return new Date(y, m - totalMonths, 1).getTime();
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            m = date.getMonth();
+                        return new Date(y, m - totalMonths, 1).getTime();
                     }
 
                     function getLastDayPreviousMonth(totalMonths = 1) {
-                    var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-                    return new Date(y, m - totalMonths + 1, 0).getTime();
+                        var date = new Date(),
+                            y = date.getFullYear(),
+                            m = date.getMonth();
+                        return new Date(y, m - totalMonths + 1, 0).getTime();
                     }
 
                     function getFirstDayInYear() {
-                    var date = new Date(), y = date.getFullYear();
-                    return new Date(y, 0, 1).getTime();
+                        var date = new Date(),
+                            y = date.getFullYear();
+                        return new Date(y, 0, 1).getTime();
                     }
+
 
                     $(document).ready(function() {
                         $.ajax({
@@ -99,20 +142,17 @@
                                 var data = JSON.parse(response);
                                 for (var i = 0; i < data.length; i++) {
                                     var tanggal = new Date(data[i].tanggal).getTime();
-                                    down = [tanggal, data[i].total_download]                   
-                                    up = [tanggal, data[i].total_upload]    
+                                    down = [tanggal, data[i].total_download]
+                                    up = [tanggal, data[i].total_upload]
                                     download.push(down);
                                     upload.push(up);
                                     down = [], up = []
                                 }
-                                console.log(download);
-                                console.log(upload);
 
                             },
                         }).done(function() {
                             var options = {
-                                series: [
-                                    {
+                                series: [{
                                         name: "download",
                                         data: download
                                     },
@@ -122,112 +162,120 @@
                                     }
                                 ],
                                 chart: {
-                                id: 'area-datetime',
-                                type: 'line',
-                                height: 420,                                    
-                                zoom: {
-                                    autoScaleYaxis: true
-                                }
+                                    id: 'area-datetime',
+                                    type: 'line',
+                                    height: 420,
+                                    zoom: {
+                                        autoScaleYaxis: true
+                                    }
                                 },
                                 colors: ['#4169E1', '#FF0000'],
-                                annotations: {
-                                },
+                                annotations: {},
                                 dataLabels: {
-                                enabled: true
+                                    enabled: true
                                 },
                                 markers: {
-                                size: 0,
-                                style: 'hollow',
+                                    size: 0,
+                                    style: 'hollow',
                                 },
                                 xaxis: {
-                                title: {
-                                text: 'Daily (Harian)'
-                                },
-                                type: 'datetime',
-                                min: getFirstDayPreviousMonth(),
-                                max: getLastDayInMonth(),
-                                tickAmount: 6,
+                                    title: {
+                                        text: 'Daily (Harian)'
+                                    },
+                                    type: 'datetime',
+                                    min: getFirstDayPreviousMonth(),
+                                    max: getLastDayInMonth(),
+                                    tickAmount: 6,
                                 },
                                 yaxis: {
-                                title: {
-                                    text: 'Bandwidth'
-                                },
-                                min: 0,
-                                max: 300
+                                    title: {
+                                        text: 'Bandwidth'
+                                    },
+                                    min: 0,
+                                    max: 300
                                 },
                                 tooltip: {
-                                x: {
-                                    format: 'dd MMM yyyy'
-                                }
+                                    x: {
+                                        format: 'dd MMM yyyy'
+                                    }
                                 },
-                                };   
+                            };
 
                             var chart = new ApexCharts(document.querySelector("#chart-timeline"), options);
                             chart.render();
-                                
-                            
+
                             var resetCssClasses = function(activeEl) {
-                            var els = document.querySelectorAll('button')
-                            Array.prototype.forEach.call(els, function(el) {
-                            el.classList.remove('active')
-                            })
-                        
-                            activeEl.target.classList.add('active')
+                                var els = document.querySelectorAll('button')
+                                Array.prototype.forEach.call(els, function(el) {
+                                    el.classList.remove('active')
+                                })
+
+                                activeEl.target.classList.add('active')
                             }
 
                             document
                                 .querySelector('#one_months')
                                 .addEventListener('click', function(e) {
-                                resetCssClasses(e)
-                            
-                                chart.zoomX(
-                                    getFirstDayInMonth(),
-                                    getLastDayInMonth()
-                                )
+                                    resetCssClasses(e)
+
+                                    chart.zoomX(
+                                        getFirstDayInMonth(),
+                                        getLastDayInMonth()
+                                    )
                                 })
-                            
+
+
+
                             document
                                 .querySelector('#three_months')
                                 .addEventListener('click', function(e) {
-                                resetCssClasses(e)
-                            
-                                chart.zoomX(
-                                    getFirstDayPreviousMonth(3),
-                                    getLastDayInMonth()
-                                )
+                                    resetCssClasses(e)
+
+                                    chart.zoomX(
+                                        getFirstDayPreviousMonth(3),
+                                        getLastDayInMonth()
+                                    )
                                 })
 
                             document
-                            .querySelector('#six_months')
-                            .addEventListener('click', function(e) {
-                            resetCssClasses(e)
-                        
-                            chart.zoomX(
-                                getFirstDayPreviousMonth(6),
-                                getLastDayInMonth()
-                            )
-                            })
-                            
+                                .querySelector('#six_months')
+                                .addEventListener('click', function(e) {
+                                    resetCssClasses(e)
+
+                                    chart.zoomX(
+                                        getFirstDayPreviousMonth(6),
+                                        getLastDayInMonth()
+                                    )
+                                })
+
                             document
                                 .querySelector('#one_year')
                                 .addEventListener('click', function(e) {
+                                    resetCssClasses(e)
+                                    chart.zoomX(
+                                        getFirstDayPreviousMonth(12),
+                                        getLastDayInMonth()
+                                    )
+                                })
+
+                            document.querySelector('#all').addEventListener('click', function(e) {
                                 resetCssClasses(e)
+
                                 chart.zoomX(
-                                    getFirstDayPreviousMonth(12),
+                                    undefined,
                                     getLastDayInMonth()
                                 )
                             })
-                            
-                            document.querySelector('#all').addEventListener('click', function(e) {
-                                resetCssClasses(e)
-                            
+
+                            document.querySelector('#select').addEventListener('change', function(e) {
                                 chart.zoomX(
-                                undefined,
-                                getLastDayInMonth()
+                                    getFirstDateOfMonth(this.value),
+                                    getLastDateOfMonth(this.value)
                                 )
                             })
 
                         });
+
                     })
                 </script>
         </div>
